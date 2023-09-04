768ed66ce7c5a682f87941078ea7a941c2701c97
haga4880 <86660297+haga4880@users.noreply.github.com>
2023-09-04 11:01:29 +0900

    Update client.ts

--- src/server/api/client.ts
+++ src/server/api/client.ts
@@ -1,9 +1,10 @@
 import { createClient } from 'microcms-js-sdk'; //ES6
 
+const ctx = useRuntimeConfig();
 
 const client = createClient({
-    serviceDomain: SERVICE_DOMAIN,
-    apiKey: API_KEY,
+    serviceDomain: ctx.serviceDomain,
+    apiKey: ctx.apiKey,
 });
 
 export default client
